ab63e2f1b1b76f21efc0ab7533a6209a650c3a65
matthew-js-porter <matthew.js.porter@jpmchase.com>
2023-09-22 13:52:37 -0400

    adding start of project.

---
+++ src/main/java/com/example/loomdemo/LoomDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.loomdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoomDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoomDemoApplication.class, args);
+	}
+
+}
